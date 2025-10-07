a33b41934ded8737896dc8c578fb73ce6a2019d6
Minh Truong <tmtruong25@gmail.com>
2025-10-07 14:11:44 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php 
 // $con=mysqli_connect('localhost','root','','ecommerce_1');
-$con = new mysqli('localhost','root','','ecommerce_1');
+$con = new mysqli('54.255.146.95','tmdt_user','Nt25102004@','ecommerce_tmdt');
 if(!$con){
     die(mysqli_error($con));
 }
